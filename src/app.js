ac11b8d7657465a8038ced6720c79323df954230
Amanda Dienstmann <amanda.dienstmann@sap.com>
2026-06-10 22:12:16 -0300

    include module Task and routes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const swaggerSpec = require('./config/swagger')
 const errorHandler = require('./middlewares/errorHandler')
 const AppError = require('./utils/AppError')
 const authRoutes = require('./routes/authRoutes')
-
 const userRoutes = require('./routes/userRoutes')
+const taskRoutes = require('./routes/taskRoutes')
 
 const app = express()
 
@@ -13,15 +13,14 @@ app.use(express.json())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-//rotassss
 app.use('/auth', authRoutes)
-
 app.use('/users', userRoutes)
+app.use('/tasks', taskRoutes)
 
-app.use((req, res, next) => { //handler de 404; se chegou aqui, nenhuma rota que tá acima disso deu certo 
+app.use((req, res, next) => {
   next(new AppError(`Rota ${req.method} ${req.url} não encontrada`, 404))
 })
 
-app.use(errorHandler) //registra o error handler criado, é o next do erro 
+app.use(errorHandler)
 
 module.exports = app
